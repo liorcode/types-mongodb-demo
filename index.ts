a20dc877ca734908652ae0221c716df9f8740c4d
Lior Chen <liorc@wix.com>
2020-08-16 18:31:49 +0300

    chore: add another example

--- index.ts
+++ index.ts
@@ -10,3 +10,12 @@ client.collection<Doc>('col').find({
   'accounts.pageId': {$in: [1, 2, 3]},
 }, {projection: {'accounts.pageId': 1}})
   .map(user => user.accounts.pageId);
+
+async function getUserPages() {
+  const users = await client.collection<Doc>('users').find({
+      'account.pageId': {$in: [1, 2, 3]},
+    }, {projection: {'account.pageId': 1}})
+      .toArray();
+
+  return users.map(user => user.account.pageId)
+}
